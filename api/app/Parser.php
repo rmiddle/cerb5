746a9101b9da0a006e5bdadba70d9445d7ac5bbf
rmiddle <robert@middleswarth.net>
2009-08-06 11:23:58 -0400

    Fixed fubar Patch http://github.com/wgm/cerb4/commit/5cc815cf0b4c54e9258da315503d5a7c58834b71

--- api/app/Parser.php
+++ api/app/Parser.php
@@ -372,20 +372,6 @@ class CerberusParser {
 		$sTo = @$headers['to'];
 		$bIsNew = true;
 
-		// Overloadable
-		$enumSpamTraining = '';
-
-		$from = array();
-		$to = array();
-
-		if(!empty($sReplyTo)) {
-			$from = CerberusParser::parseRfcAddress($sReplyTo);
-		} elseif(!empty($sFrom)) {
-			$from = CerberusParser::parseRfcAddress($sFrom);
-		} elseif(!empty($sReturnPath)) {
-			$from = CerberusParser::parseRfcAddress($sReturnPath);
-		}
-
 		if(!empty($sTo)) {
 		    // [TODO] Do we still need this RFC address parser?
 			$to = CerberusParser::parseRfcAddress($sTo);
@@ -406,23 +392,14 @@ class CerberusParser {
 		if(empty($iDate) || $iDate > time())
 			$iDate = time();
 
-		if(empty($from) || !is_array($from)) {
-			$logger->warn("[Parser] Invalid 'From' address: " . $from);
-			return NULL;
-		}
-
-		@$fromAddress = $from[0]->mailbox.'@'.$from[0]->host;
-		@$fromPersonal = $from[0]->personal;
-		if(null == ($fromAddressInst = CerberusApplication::hashLookupAddress($fromAddress, true))) {
-			$logger->err("[Parser] 'From' address could not be created: " . $fromAddress);
-			return NULL;
-		}
-
 		// Is banned?
 		if(1==$fromAddressInst->is_banned) {
 			$logger->info("[Parser] Ignoring ticket from banned address: " . $fromAddressInst->email);
 			return NULL;
 		}
+
+    // Overloadable
+    $enumSpamTraining = '';
 
 		// Message Id / References / In-Reply-To
 		@$sMessageId = $headers['message-id'];
@@ -530,36 +507,7 @@ class CerberusParser {
         	}
         }
 
-        // Pre-parse mail rules
-        if(null != ($pre_filter = Model_PreParseRule::getMatches(
-        	(empty($id) ? 1 : 0), // is_new
-        	$fromAddressInst,
-        	$message
-        ))) {
-        	// Do something with matching filter's actions
-        	foreach($pre_filter->actions as $action_key => $action) {
-        		switch($action_key) {
-        			case 'blackhole':
-        				return NULL;
-        				break;
-
-        			case 'redirect':
-        				@$to = $action['to'];
-        				CerberusMail::reflect($message, $to);
-        				return NULL;
-        				break;
-
-        			case 'bounce':
-        				@$msg = $action['message'];
-        				// [TODO] Follow the RFC spec on a true bounce
-        				CerberusMail::quickSend($fromAddress,"Delivery failed: ".$sSubject,$msg);
-        				return NULL;
-        				break;
-        		}
-        	}
-        }
-
-		$group_id = 0;
+    $group_id = 0;
 
 		if(empty($id)) { // New Ticket
 			$sMask = CerberusApplication::generateTicketMask();
