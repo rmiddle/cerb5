3d3f288de10a7213d064dab94cf2d28613263e01
rmiddle <robert@middleswarth.net>
2010-02-27 09:00:46 -0500

    Ignore banned emails from Email Auto-Complete.

--- features/cerberusweb.core/api/uri/contacts.php
+++ features/cerberusweb.core/api/uri/contacts.php
@@ -1225,9 +1225,10 @@ class ChContactsPage extends CerberusPageExtension {
 		
 		$sql = sprintf("SELECT first_name,last_name,email ".
 			"FROM address ".
-			"WHERE lower(email) LIKE %s ".
+			"WHERE (lower(email) LIKE %s ".
 			"OR lower(concat(first_name,' ',last_name)) LIKE %s ".
-			"OR lower(last_name) LIKE %s ".
+			"OR lower(last_name) LIKE %s) ".
+			"AND is_banned = 0 ".                                    
 			"ORDER BY first_name, last_name, email",
 			$db->qstr($starts_with),
 			$db->qstr($starts_with),
